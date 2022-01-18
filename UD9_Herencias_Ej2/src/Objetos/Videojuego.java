504bb694b3df847d81e6f697d090bcb0399df141
JoanR17 <joanr.m.1999@gmail.com>
2022-01-18 09:24:22 +0100

    Serie/VideoJuego

---
+++ UD9_Herencias_Ej2/src/Objetos/Videojuego.java
@@ -0,0 +1,74 @@
+package Objetos;
+
+public class Videojuego {
+	
+	private String titulo, genero, compania;
+	private int horasEstimadas;
+	private boolean entregado;
+	
+	public Videojuego() 
+	{
+		this.titulo = "";
+		this.genero = "";
+		this.compania = "";
+		this.horasEstimadas = 10;
+		this.entregado = false;
+	}
+	
+	public Videojuego(String titulo, int horasEstimadas) 
+	{
+		this.titulo = titulo;
+		this.genero = "";
+		this.compania = "";
+		this.horasEstimadas = horasEstimadas;
+		this.entregado = false;
+	}
+	
+	public Videojuego(String titulo, String genero, String compania, int horasEstimadas) 
+	{
+		this.titulo = titulo;
+		this.genero = genero;
+		this.compania = compania;
+		this.horasEstimadas = horasEstimadas;
+		this.entregado = false;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCompania() {
+		return compania;
+	}
+
+	public void setCompania(String compania) {
+		this.compania = compania;
+	}
+
+	public int getHorasEstimadas() {
+		return horasEstimadas;
+	}
+
+	public void setHorasEstimadas(int horasEstimadas) {
+		this.horasEstimadas = horasEstimadas;
+	}
+
+	@Override
+	public String toString() {
+		return "Videojuego [titulo=" + titulo + ", genero=" + genero + ", compania=" + compania + ", horasEstimadas="
+				+ horasEstimadas + ", entregado=" + entregado + "]";
+	}
+	
+}
