b269b1b00649e9fd0ed254dfdddf6371dd4bf3a3
Adrytan710 <adryrf16@gmail.com>
2022-01-19 12:34:51 +0100

    Commit

--- UD9_Herencias_Ej4/src/Ejercicio4/Raices.java
+++ UD9_Herencias_Ej4/src/Ejercicio4/Raices.java
@@ -1,16 +1,26 @@
 package Ejercicio4;
 
+/**
+ * @author Adrián Rodriguez
+ */
+
 public class Raices {
 
+	//Atributos
+	
 	private double a;
 	private double b;
 	private double c;
+	
+	//Constructores
 	
 	public Raices(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
+	
+	//Método obtenerRaices
 	
 	public void obtenerRaices() {
 		double raiz1 = (-b + Math.sqrt(getDiscriminate())) / (2*a);
@@ -20,23 +30,33 @@ public class Raices {
 		System.out.println("La segunda solución es: " + raiz2);
 	}
 	
+	//Método obtenerRaiz
+	
 	public void obtenerRaiz() {
 		double raiz = (-b) / (2*a);
 		
 		System.out.println("La solución es: " + raiz);
 	}
 	
+	//Getter Discriminate
+	
 	public double getDiscriminate() {
 		return Math.pow(b, 2) - (4*a*c);
 	}
+	
+	//Método tieneRaices
 	
 	public boolean tieneRaices() {
 		return getDiscriminate() > 0;
 	}
 	
+	//Método tieneRaiz
+	
 	public boolean tieneRaiz() {
 		return getDiscriminate() == 0;
 	}
+	
+	//Método calcular
 	
 	public void calcular() {
 		if (tieneRaices()) {
---
+++ UD9_Herencias_Ej4/src/Main/MainApp.java
@@ -0,0 +1,20 @@
+package Main;
+
+import Ejercicio4.Raices;
+
+/**
+ * @author Adrián Rodriguez
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		//Objeto con valores añadidos
+		Raices r = new Raices(2, 12, 4);
+		
+		//Calculo
+		
+		r.calcular();
+	}
+}
